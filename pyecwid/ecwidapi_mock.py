e946d13121dd7e29a7e5e7af323bbdaf4a73b68e
Dan <63722790+DanPalmz@users.noreply.github.com>
2021-03-14 23:31:19 +1100

    Add mocks for put post delete requests and results

--- pyecwid/ecwidapi_mock.py
+++ pyecwid/ecwidapi_mock.py
@@ -1,4 +1,5 @@
 import urllib.parse
+#import responses
 from pyecwid.validators import paramater_validators as validator
 
 API_BASE_URL = 'https://ecwid.localhost/api/v3/{0}/'
@@ -19,17 +20,57 @@ class EcwidAPIMock:
         self.base_url = base_url.format(store_id)
         self.debug = DEBUG
 
+
+    def delete_request(self, endpoint):
+        url = self.__get_feature_url(endpoint)
+
+        payload = {'token': self.api_token}
+
+        print(f'DELETE request: {url} with {payload}')
+
+        result = RequestResult(200, {'deleteCount': 1})
+        return result
+        
     def get_request(self, endpoint, payload={}):
         feature_url = self.__get_feature_url(endpoint)
 
         payload['token'] = self.api_token
         payload['limit'] = API_PAGE_LIMIT
 
-        print(f'Sending request to {feature_url} with {payload}')
+        print(f'GET request: {feature_url} with {payload}')
 
     def get_base_url(self):
         return(self.base_url)
+    
+    def post_request(self, endpoint, values):
+        url = self.__get_feature_url(endpoint)
+        payload = {'token': self.api_token}
+        
+        print(f'POST request: {url} with {payload}. \nValues:')
+        print(values)
+
+        result = RequestResult(200, {'id': 1234})
+        return result
+
+    def put_request(self, endpoint, values):
+        url = self.__get_feature_url(endpoint)
+        payload = {'token': self.api_token}
+
+        print(f'PUT request: {url} with {payload}. \nValues:')
+        print(values)
+
+        result = RequestResult(200)
+        return result
 
     def __get_feature_url(self, endpoint):
         feature_url = urllib.parse.urljoin(self.base_url, endpoint)
         return feature_url
+
+class RequestResult:
+    def __init__(self, result, json={}):
+        self.status_code = result
+        if json:
+            self.json_value = json
+
+    def json(self):
+        return self.json_value
